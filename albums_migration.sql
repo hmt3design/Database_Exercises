30ec38ca85605b5a4e44c6ea378db3dd4ba82b33
hmt3design <hmt3design@gmail.com>
2017-01-24 11:04:53 -0600

    012417 - albums_migration.sql exercise

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+use codeup_test_db;
+DROP DATABASE IF EXISTS albums;
+CREATE TABLE albums(
+id int(10) unsigned not null auto_increment,
+artist varchar(300) not null,
+name VARCHAR(300) NOT NULL,
+release_date YEAR,
+sales DECIMAL(10),
+genre VARCHAR(300),
+primary key(id)
+);
