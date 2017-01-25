624b8888a355cd405309e536a720052cd4066e52
hmt3design <hmt3design@gmail.com>
2017-01-25 09:14:40 -0600

    012517 - finish CRUD exercises (correct missing entry)

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,6 +1,6 @@
 SELECT 'Delete albums released after 1991' AS 'Exercise 1';
 DELETE FROM albums WHERE release_date > 1991;
 SELECT 'Delete albums with the genre of Disco' AS 'Exercise 2';
-DELETE FROM albums WHERE genre = 'Disco';
+DELETE FROM albums WHERE genre LIKE 'Disco';
 SELECT 'Delete albums by Peter Jackson' AS 'Exercise 3';
-
+DELETE FROM albums WHERE artist = 'Peter Jackson';
