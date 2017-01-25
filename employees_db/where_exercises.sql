7f00b7108afe3f0217b0698e1714b1647193e0c3
hmt3design <hmt3design@gmail.com>
2017-01-25 12:22:16 -0600

    012517 - Where exercises, part 2

--- employees_db/where_exercises.sql
+++ employees_db/where_exercises.sql
@@ -1,5 +1,6 @@
-SELECT emp_no, first_name, last_name FROM employees.employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
-SELECT emp_no, first_name, last_name FROM employees.employees WHERE last_name LIKE 'E%';
-SELECT emp_no, first_name, last_name, hire_date FROM employees.employees WHERE hire_date LIKE '199%';
-SELECT emp_no, first_name, last_name, birth_date FROM employees.employees WHERE birth_date LIKE '%12-25';
-SELECT emp_no, first_name, last_name FROM employees.employees WHERE last_name LIKE '%q%';
+SELECT * FROM employees.employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya');
+SELECT * FROM employees.employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M';
+SELECT * FROM employees.employees WHERE last_name LIKE 'E%' OR last_name LIKE '%e';
+SELECT * FROM employees.employees WHERE last_name LIKE 'E%' AND last_name LIKE '%e';
+SELECT * FROM employees.employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%12-25';
+SELECT * FROM employees.employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
